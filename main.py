29e5976a1689f254df3c25d9acaedee7b5f2d060
Jinqu3 <132920100+Jinqu3@users.noreply.github.com>
2023-08-18 16:43:49 +0400

    registation
    added registration system

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 from aiogram.dispatcher.filters.state import StatesGroup,State
 from aiogram.dispatcher import FSMContext
 
-from sqlite import db_start,create_profile_db,edit_profile_db
+import sqlite as sq
+from config import TOKEN
 
-
-TOKEN = "YOUR TOKEN"
 
 storage = MemoryStorage()
 bot = Bot(token=TOKEN)
@@ -16,6 +15,8 @@ dp = Dispatcher(bot=bot,storage=storage)
 
 class ProfileStatesGroup(StatesGroup):
     
+    registration = State()
+    login = State()
     photo = State()
     name = State()
     age = State()
@@ -24,7 +25,7 @@ class ProfileStatesGroup(StatesGroup):
 """
 Вспомогательные функции
 """
-def get_keyboard()->ReplyKeyboardMarkup:
+def create_keyboard()->ReplyKeyboardMarkup:
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     kb.add(KeyboardButton("/create"))
     return kb
@@ -34,21 +35,25 @@ def get_cancel()->ReplyKeyboardMarkup:
     kb.add(KeyboardButton("/cancel"))
     return kb
 
+def get_keyboard()->ReplyKeyboardMarkup:
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(KeyboardButton("/registr"),KeyboardButton("/login"))
+    return kb
+
 async def on_startup(_):
-    await db_start()
+    await sq.db_start()
 """
 Хендлеры запуска/вспомогательные
 """
 @dp.message_handler(commands=['start'])
 async def start_message(message: types.Message)->None:
-    start_text = "Добро пожаловать!\nЧтобы создать профиль,введите команду /create"
+    start_text = "Добро пожаловать!\nДля начала нужно выполнить вход,для этого введите команду /registr или /login"
     await message.answer(text = start_text,
                         reply_markup=get_keyboard())
     await message.delete()
 
 @dp.message_handler(commands=['create'])
-async def create_profile(message:types.Message):
-    await create_profile_db(message.from_user.id)   
+async def create_profile(message:types.Message):   
     await message.reply(text= "Давай создадим твой профиль.Для начала отправь своё фото",
                            reply_markup=get_cancel())
     await ProfileStatesGroup.photo.set()
@@ -62,6 +67,38 @@ async def cancel(message : types.Message,state :FSMContext):
         return
     await message.answer("Отменил регистрацию",reply_markup=get_keyboard())
     await state.finish()
+"""
+Хендлеры Регистрации
+"""
+@dp.message_handler(commands=['registr'],state=None)
+async def registration(message:types.Message):
+    if await sq.find_user(message.from_user.id):
+        await message.reply("Ты уже зарегистрирован,для входа введи команду /login",reply_markup=get_keyboard())
+    else:
+        await message.reply("Придумай себе пароль",reply_markup=get_cancel())
+        await ProfileStatesGroup.registration.set()
+
+@dp.message_handler(state=ProfileStatesGroup.registration)
+async def registration(message:types.Message,state : FSMContext):
+    await sq.create_profile_db(message.from_user.id,message.text)
+    await message.answer("Регистрация прошла успешно,теперь ты можешь настроить свой профиль,для этого напиши команду /create",reply_markup=create_keyboard())
+    await state.finish()
+
+@dp.message_handler(commands=['login'],state=None)
+async def registration(message:types.Message):
+    if await sq.find_user(message.from_user.id):
+        await ProfileStatesGroup.login.set()
+        await message.reply("Введи пароль",reply_markup=get_cancel())
+    else:
+        await message.reply("Тебе нужно зарегистрироваться,для этого введи команду /registr",reply_markup=get_keyboard())
+    
+@dp.message_handler(state=ProfileStatesGroup.login)
+async def login(message:types.Message,state : FSMContext):
+    if message.text != await sq.get_password(message.from_user.id):
+        await message.answer("Неправильный пароль,попробуй ещё раз",reply_markup=get_cancel())
+    else:
+        await message.answer("Вход прошёл успешно,теперь ты можешь настроить свой профиль,для этого напиши команду /create",reply_markup=create_keyboard())
+        await state.finish()
 """
 Хедлер ввода фото
 """
@@ -117,7 +154,7 @@ async def load_desc(message:types.Message,state:FSMContext):
         data['desc'] = message.text #сохраням описание
         await message.reply(f'Хорошо,я тебя запомнил')
         await bot.send_photo(message.from_user.id,data['photo'],caption=f"Тебя зовут {data['name']},тебе {data['age']} лет\nВот немного информации  о тебе: {data['desc']}")
-        await edit_profile_db(data,message.from_user.id)
+        await sq.edit_profile_db(data,message.from_user.id)
     await state.finish()
 
 if __name__ == "__main__":
--- sqlite.py
+++ sqlite.py
@@ -1,20 +1,26 @@
 import sqlite3
+from config import path_db
 
 async def db_start():
     global db,cur
 
-    db = sqlite3.connect('new.db')
+    db = sqlite3.connect(path_db)
     cur = db.cursor()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS profile(user_id TEXT PRIMARY KEY,photo TEXT,age TEXT,description TEXT,name TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS profile(user_id INT PRIMARY KEY,photo TEXT,age TEXT,description TEXT,name TEXT,password TEXT)")
     
     db.commit()
 
-async def create_profile_db(user_id):
-    user = cur.execute("SELECT user_id FROM profile WHERE user_id == ?",(user_id)).fetchone()
-    if not user:
-        cur.execute("INSERT INTO profile VALUES(?,?,?,?,?)",(user_id,'','','',''))
+async def create_profile_db(user_id,password):
+    if not await find_user(user_id):
+        cur.execute("INSERT INTO profile VALUES(?,?,?,?,?,?)",(user_id,'','','','',password))
         db.commit()
+
+async def get_password(user_id):
+    return cur.execute("SELECT password FROM profile WHERE user_id == ?",(user_id,)).fetchone()[0]
+
+async def find_user(user_id):
+    return cur.execute("SELECT user_id FROM profile WHERE user_id == ?",(user_id,)).fetchone()
     
 async def edit_profile_db(data,user_id):
     cur.execute("UPDATE profile SET photo = ? ,age = ? ,description = ? ,name = ? WHERE user_id == ?",
